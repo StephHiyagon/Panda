d97eaf64cae49f97d38873013230357776f3efe0
stephHiyagon <mv.hiyagon@gmail.com>
2017-04-17 15:50:22 -0500

    agregando js

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,3 +20,27 @@ function ocultar2(event){
   foto2.style.display="none";
   span2.style.display="none";
 }
+
+var restaura=document.getElementById('restaurar');
+
+restaura.addEventListener('click',function(){
+  foto1.style.display="block";
+  span1.style.display="block";
+  foto2.style.display="block";
+  span2.style.display="block";
+  foto3.style.display="block";
+  span3.style.display="block";
+  foto4.style.display="block";
+  span4.style.display="block";
+});
+
+var origen=document.getElementById('origen');
+
+origen.addEventListener('click',function(){
+  var parrafo1=document.getElementById('historiaP');
+  if(parrafo1.style.visibility=="visible"){
+    parrafo1.style.visibility="hidden";
+  } else {
+    parrafo1.style.display="visible";
+  }
+});
