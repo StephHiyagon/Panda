ca76dda8605e78c0163e1603441275c7f1941489
stephHiyagon <mv.hiyagon@gmail.com>
2017-04-17 15:23:31 -0500

    completo html

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,21 @@ var foto1=document.getElementById('panda1');
 var foto2=document.getElementById('panda2');
 var foto3=document.getElementById('panda3');
 var foto4=document.getElementById('panda4');
+
+var span1=document.getElementById('p1');
+var span2=document.getElementById('p2');
+var span3=document.getElementById('p3');
+var span4=document.getElementById('p4');
+
+span1.addEventListener('click',ocultar1);
+span2.addEventListener('click',ocultar2);
+
+function ocultar1(event){
+  foto1.style.display="none";
+  span1.style.display="none";
+}
+
+function ocultar2(event){
+  foto2.style.display="none";
+  span2.style.display="none";
+}
