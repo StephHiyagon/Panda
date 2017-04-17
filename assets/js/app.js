95ade8b7a29d5e0dbdd0fc2bfeae9995b8a4d1be
stephHiyagon <mv.hiyagon@gmail.com>
2017-04-17 14:51:24 -0500

    con fotos

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,4 @@
-alert("funciona!");
+var foto1=document.getElementById('panda1');
+var foto2=document.getElementById('panda2');
+var foto3=document.getElementById('panda3');
+var foto4=document.getElementById('panda4');
