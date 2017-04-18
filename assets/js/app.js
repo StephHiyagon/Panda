beb10a76cb02d9d5df9484e94076f64298d9c088
stephHiyagon <mv.hiyagon@gmail.com>
2017-04-18 09:15:30 -0500

    factorizando código js y html

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,43 +1,39 @@
-
-var div1=document.getElementById('div1');
-var div2=document.getElementById('div2');
-var div3=document.getElementById('div3');
-var div4=document.getElementById('div4');
-
+var divs=document.getElementsByClassName('divs');
 var images=document.getElementById('images');
-images.addEventListener('click',ocultar);
+var restaura=document.getElementById('restaurar');
+var origen=document.getElementById('origen');
+var parrafo1=document.getElementById('historiaP');
+var historia=document.getElementById('historia');
+var parrafo2=document.getElementById('origenP');
 
 function ocultar(event){
   if(event.target.tagName=="SPAN"){
   // console.log(event.target.id);
   switch (event.target.id) {
     case "p1":
-      div1.style.display="none";
+      divs[0].style.display="none";
       break;
     case "p2":
-      div2.style.display="none";
+      divs[1].style.display="none";
       break;
     case "p3":
-      div3.style.display="none";
+      divs[2].style.display="none";
       break;
     case "p4":
-      div4.style.display="none";
+      divs[3].style.display="none";
       break;
     }
   }
 }
 
-var restaura=document.getElementById('restaurar');
 restaura.addEventListener('click',function(){
-  var divs=document.getElementsByClassName('divs');
   for(var i=0;i<4;i++){
     divs[i].style.display="inline-block";
   }
 
 });
 //comienzan la interacción con los botones
-var origen=document.getElementById('origen');
-var parrafo1=document.getElementById('historiaP');
+
 origen.addEventListener('click',function(){
     if(parrafo1.style.visibility=="hidden"){
     parrafo1.style.visibility="visible";
@@ -46,8 +42,6 @@ origen.addEventListener('click',function(){
   }
 });
 
-var historia=document.getElementById('historia');
-var parrafo2=document.getElementById('origenP');
 historia.addEventListener('click',function(){
     if(parrafo2.style.visibility=="hidden"){
     parrafo2.style.visibility="visible";
@@ -55,3 +49,4 @@ historia.addEventListener('click',function(){
     parrafo2.style.visibility="hidden";
   }
 })
+images.addEventListener('click',ocultar);
